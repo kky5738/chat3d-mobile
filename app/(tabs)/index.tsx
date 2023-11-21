0dce28911966fc8522902c317b821652012e3042
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-11-21 18:30:02 +0900

    Feat: 텍스트 입력 기능 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <TextInput
+        style={styles.inputPrompt}
+        value={searchTerm}
+        onChangeText={(text) => setSearchTerm(text)}
+        placeholder='Test'
+      />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
@@ -27,5 +35,10 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  inputPrompt: {
+    width: "60%",
+    height: "10%",
+    paddingHorizontal: 16
   },
 });
