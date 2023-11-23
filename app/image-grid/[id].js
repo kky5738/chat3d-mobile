f3f7091245cb0458a2a8bc034605b83b27542224
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-11-23 11:36:10 +0900

    Feat: image creat 연결 확인   - 서버에서 image를 받기 위해 연결 테스트 진행

--- app/image-grid/[id].js
+++ app/image-grid/[id].js
@@ -1,15 +1,28 @@
-import React from "react";
+import {react, useState, useCallback} from "react";
 import { View, Text } from "react-native";
 import { useGlobalSearchParams} from 'expo-router'
 import useFetch from '../../Hook/useFetch'
 
-
+// fix branch 만들고 Hook/useFetch 에서 params 대신 data로 수정하여 request body로 만들기
 const ImageGrid = () => {
     const params = useGlobalSearchParams()
-    
+    const {data, isLoading, error, refetch} = useFetch("image-create", "POST", {
+        query: params.id,
+        modelName: "stable diffusion"
+    })
+    const [refreshing, setRefreshing] = useState(false)
+    const onRefresh = useCallback(() => {
+        setRefreshing(true)
+        refetch()
+        setRefreshing(false)
+    }, [])
+
     return (
         <View>
             <Text>ImageGrid Component test</Text>
+            <View>
+                <Text>{data}</Text>
+            </View>
         </View>
     )
 }
