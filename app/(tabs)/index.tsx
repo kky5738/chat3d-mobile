7fe8c1609376847b74dd3360653664af86d2a7fd
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-11-24 19:30:43 +0900

    Update code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,37 +11,37 @@ export default function TabOneScreen() {
   const [prompt, setPrompt] = useState(Object)
 
   // When server is working on
-  // const handlePress = async () => {
+  const handlePress = async () => {
 
-  //   console.log("send pressed", inputText)
-  //   const options = {
-  //     method: "POST",
-  //     url: `http://100.64.159.131:8375/text`,
-  //     data: {
-  //       query: inputText,
-  //       model_id: "T5"
-  //     }
-  //   }
+    console.log("send pressed", inputText)
+    const options = {
+      method: "POST",
+      url: `http://100.64.159.131:8375/text`,
+      data: {
+        query: inputText,
+        modelName: "T5"
+      }
+    }
 
-  //   try{
-  //     const response = await axios.request(options)
-  //     console.log(response.data.answer)
-  //     setPrompt(response.data.answer)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
+    try{
+      const response = await axios.request(options)
+      console.log(response.data.answer)
+      setPrompt(response.data.answer)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   // When server is not working
-  const handlePress = () => {
-    const answerArray = [
-      'A minimalist table: This design features a clean and uncluttered look, making it perfect for small spaces or as an accent piece in larger rooms. It is made of high-quality materials that will not take up too much space while still providing ample surface area to spread out your dinnerware',
-      'A minimalist table with a clean and simple design: This type of furniture is often made from natural materials such as wood or metal, giving it an industrial look. The tables are usually designed to be functional but not necessarily decorative in nature; they typically have minimal details that add visual interest without taking up too much space on the floor',
-      'A minimalist table: This design is characterized by its clean lines and simple shapes, making it perfect for small spaces or as an accent piece in larger rooms. The tables are made of high-quality materials that have been carefully chosen to blend seamlessly with the surrounding decor while still maintaining their own unique aesthetics'
-    ]
+  // const handlePress = () => {
+  //   const answerArray = [
+  //     'A minimalist table: This design features a clean and uncluttered look, making it perfect for small spaces or as an accent piece in larger rooms. It is made of high-quality materials that will not take up too much space while still providing ample surface area to spread out your dinnerware',
+  //     'A minimalist table with a clean and simple design: This type of furniture is often made from natural materials such as wood or metal, giving it an industrial look. The tables are usually designed to be functional but not necessarily decorative in nature; they typically have minimal details that add visual interest without taking up too much space on the floor',
+  //     'A minimalist table: This design is characterized by its clean lines and simple shapes, making it perfect for small spaces or as an accent piece in larger rooms. The tables are made of high-quality materials that have been carefully chosen to blend seamlessly with the surrounding decor while still maintaining their own unique aesthetics'
+  //   ]
 
-    setPrompt(answerArray)
-  }
+  //   setPrompt(answerArray)
+  // }
 
   return (
     <View style={styles.container}>
