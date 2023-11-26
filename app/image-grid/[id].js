70f7d4f94c32af0436d0d6427156b314e68f939b
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-11-26 22:54:30 +0900

    Update styles
    - 서버에서 생성된 이미지를 중앙에 배치하는 style 코드 적용

--- app/image-grid/[id].js
+++ app/image-grid/[id].js
@@ -1,7 +1,8 @@
 import React, { useState, useCallback } from "react";
-import { View, Text, FlatList, Pressable, Image, ActivityIndicator } from "react-native";
+import { View, Text, FlatList, Pressable, Image, ActivityIndicator, StyleSheet } from "react-native";
 import { useGlobalSearchParams } from 'expo-router';
 import useFetch from '../../Hook/useFetch';
+import { MonoText } from '../../components/StyledText';
 
 const ImageGrid = () => {
     const params = useGlobalSearchParams();
@@ -18,17 +19,17 @@ const ImageGrid = () => {
     }, []);
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text>ImageGrid Component test</Text>
-            <View>
+            <View style={styles.imageContainer}>
                 <FlatList
                     data={data?.images}
                     renderItem={({ item }) => (
-                        <View>
+                        <View style={styles.imageWrapper}>
                             <Pressable onPress={() => router.push(`/image-grid/${item}`)}>
                                 <Image
                                     source={{ uri: `data:image/png;base64,${item}` }}
-                                    style={{ width: 100, height: 100 }}
+                                    style={styles.image}
                                     resizeMode="contain"
                                 />
                             </Pressable>
@@ -37,8 +38,9 @@ const ImageGrid = () => {
                     keyExtractor={(item, index) => index.toString()}
                     refreshing={refreshing}
                     onRefresh={onRefresh}
+                    numColumns={2}
                     ListEmptyComponent={() => (
-                        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+                        <View style={styles.emptyListContainer}>
                             {isLoading ? (
                                 <ActivityIndicator size="large" color="#0000ff" />
                             ) : (
@@ -48,7 +50,7 @@ const ImageGrid = () => {
                     )}
                     ListFooterComponent={() => (
                         isLoading && (
-                            <View style={{ alignItems: 'center', justifyContent: 'center', paddingVertical: 20 }}>
+                            <View style={styles.loadingContainer}>
                                 <ActivityIndicator size="large" color="#0000ff" />
                             </View>
                         )
@@ -58,5 +60,37 @@ const ImageGrid = () => {
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        width: '80%', // You can adjust the width as needed
+    },
+    imageWrapper: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        padding: 10,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginTop: 20,
+    },
+    emptyListContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    },
+    loadingContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 20,
+    },
+});
 
 export default ImageGrid;
