f05cb2ef4a0c956af17c0cd29d0dc17f5bfe2904
Gyeongyeong Kim <74523540+kky5738@users.noreply.github.com>
2023-11-28 16:07:39 +0900

    Feat: update backend code   - add create mesh file

--- backend/app/api.py
+++ backend/app/api.py
@@ -4,14 +4,24 @@ import base64
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from llm.Getprompt import TextProcessing_gpt, TextProcessing_T5
-from datetime import datetime
-# from diffusion import Diffuse
-import os
+from fastapi.responses import JSONResponse
 
-# from diffusers import StableDiffusionPipeline
-# from transformers import pipeline
+from pydantic import BaseModel
+from llm.Getprompt import TextProcessingGPT, TextProcessingT5
+from datetime import datetime
+from diffusion import Diffuse
+import os
+import sys
+from io import BytesIO
+from PIL import Image
+
+import mesh_with_shap_e as shapE
+
+try:
+    from rembg import remove
+except ImportError:
+    print('Please install rembg with "pip install rembg"')
+    sys.exit()
 
 origins = ["http://127.0.0.1:19006",
 "http://127.0.0.1:19006/api",
@@ -48,6 +58,11 @@ class Item(BaseModel):
     query: str
     modelName: str
 
+class Item3D(BaseModel):
+    query: str
+    ID: str
+    modelName: str
+    
 @app.get("/items/")
 async def read_item():
     
@@ -64,10 +79,11 @@ def recommend_prompt(item: Item):
     # 언어모델로 T5을 사용할 경우
     if model_name == "T5":
         # 언어모델 불러오기
-        llm_t5 = TextProcessing_T5(ner_model = 'DeveloperSejin/NER_for_furniture_3D_object_create',model_name='DeveloperSejin/Fine_Tuned_Flan-T5-large_For_Describe_Furniture')
+        llm_t5 = TextProcessingT5(ner_model = 'DeveloperSejin/NER_for_furniture_3D_object_create',model_name='DeveloperSejin/Fine_Tuned_Flan-T5-large_For_Describe_Furniture')
         
         # 언어모델을 통해 prompt 추천 받기
-        recommend, answer = llm_t5.getAnswer(prompt=query) 
+        recommend, answer = llm_t5.get_answer(prompt=query) 
+        
         
         # 메모리 관리를 위해 모델 객체 삭제
         del llm_t5
@@ -79,6 +95,12 @@ def recommend_prompt(item: Item):
         "recommend": recommend,
         "answer": answer}
 
+def image_to_base64(image):
+    img_byte_array = BytesIO()
+    image.save(img_byte_array, format="PNG")
+    img_data = img_byte_array.getvalue()
+    return base64.b64encode(img_data).decode('utf-8')    
+    
 @app.post("/image-create/")
 def create_image(item: Item):
     model_name = item.modelName
@@ -86,10 +108,51 @@ def create_image(item: Item):
     
     print(f"This query({prompt}) is requested from client")
     
-    # 여기에 이미지 생성하는 코드 구현하기
-    image = [f"image{i+1}" for i in range(4)]
+    images = []
+    for i in range(4):
+        
+        org_img = Diffuse.run2(prompt)
+        removed_img = remove(org_img, alpha_matting=False)
+        image_base64 = image_to_base64(removed_img)
+        
+        images.append(image_base64)
     
-    return {
-        "image": image,
-        "userRequest": prompt,
-        "status": "200 OK"}
+    print("image is all set")
+    response_data = {"images": images, "userReqest": prompt}
+    
+    return JSONResponse(content=response_data)
+
+@app.post("/create-3D/")
+def create_3d(item: Item3D):
+    # get params
+    image_query = item.query
+    ID = item.ID
+    model_name = item.modelName
+    
+    
+    print("request arrived")
+    
+    print("This is test of 3D creation")
+
+    print(image_query[:10])
+    # base64 디코딩
+    decoded_img = base64.b64decode(image_query)
+
+    # BytesIO를 사용하여 이미지 데이터를 메모리에 로드
+    image_stream = BytesIO(decoded_img)
+
+    # make 3D
+    shapE.create_mesh(image_stream, str(ID))
+    
+    """
+    3D 생성하고 반환을 어떻게 해줄지 고민해보기
+      1. shapE 모델로 바로 3D 생성하고 return 할지
+        1-1. shapE 모델에서 image to 3d만 이용할지
+        1-2. text to 3d도 진행해 결과를 여러개 만들지
+      2. realfusion으로 1시간? 정도 시간 소요해서 3d 생성하고 메일로 결과를 보낼지
+      3. 위 모두를 고려할지
+        3-1. single user만 고려한다면 3개 다 진행해도 상관x
+        3-2. 하지만 multi user를 고려한다면 trafic, GPU memory 등을 고려해 결정해야 함
+    """
+
+    return {"status": "200 OK"}
---
+++ backend/mesh_with_shap_e.py
@@ -0,0 +1,62 @@
+import torch
+import time
+
+from shap_e.diffusion.sample import sample_latents
+from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
+from shap_e.models.download import load_model, load_config
+from shap_e.util.notebooks import create_pan_cameras, decode_latent_images
+from shap_e.util.image_util import load_image
+
+def create_mesh(img_path: str, ID: str, render_mode='nerf'):
+    torch.cuda.empty_cache()
+    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+
+    xm = load_model('transmitter', device=device)
+    model = load_model('image300M', device=device)
+    diffusion = diffusion_from_config(load_config('diffusion'))
+    batch_size = 4
+    guidance_scale = 3.0
+
+    # To get the best result, you should remove the background and show only the object of interest to the model.
+
+    start_time = time.time()
+    
+    image = load_image(img_path)
+
+    latents = sample_latents(
+        batch_size=batch_size,
+        model=model,
+        diffusion=diffusion,
+        guidance_scale=guidance_scale,
+        model_kwargs=dict(images=[image] * batch_size),
+        progress=True,
+        clip_denoised=True,
+        use_fp16=True,
+        use_karras=True,
+        karras_steps=64,
+        sigma_min=1e-3,
+        sigma_max=160,
+        s_churn=0,
+    )
+    # render_mode = 'nerf' # you can change this to 'stf' for mesh rendering
+    render_mode = render_mode # you can change this to 'stf' for mesh rendering
+    size = 64 # this is the size of the renders; higher values take longer to render.
+
+    cameras = create_pan_cameras(size, device)
+    for i, latent in enumerate(latents):
+        images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
+
+    # Example of saving the latents as meshes.
+    from shap_e.util.notebooks import decode_latent_mesh
+
+    t = decode_latent_mesh(xm, latents[0]).tri_mesh()
+        # with open(f'example_mesh_{i}.ply', 'wb') as f: # 원본
+    with open(f'./static/{ID}/generated_3d.obj', 'w') as f:
+        t.write_obj(f)
+    
+    end_time = time.time()
+    print(f"[INFO] mesh exporting takes {(end_time - start_time)/ 60:.4f} minutes.")
+    
+if __name__ == '__main__':
+    img_path = "gyeongyeong_Kim/ImageCaptioning_with_RN/backend/shap-e/shap_e/examples/example_data/cactus.png"
+    create_mesh(img_path)
